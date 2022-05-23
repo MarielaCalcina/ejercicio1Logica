fce93b528a430e26aa8d40f41cffe8ac78c46fbd
MarielaCalcina <marielacalcinaali@gmail.com>
2022-05-23 16:18:05 -0300

    initial commit

---
+++ ejercicio1Logica/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicio1Logica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Hola mundo! !!!";
+
+            char[] fraseLista = frase.ToCharArray();
+            string fraseAlReves = string.Empty;
+            //recorrer cada palabara
+
+            for (int i = 0; i < frase.Length; i++)
+            {
+                fraseAlReves += fraseLista[frase.Length - (i + 1)];
+            }
+
+            Console.Write(fraseAlReves);
+        }
+    }
+}
